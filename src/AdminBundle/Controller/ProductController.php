70f9f757fcc00ccd2419e6b1d772c08f5e990810
pophamdi <pophamdi@gmail.com>
2018-03-03 13:26:49 +0100

    generate the Image entity add relation with product

--- src/AdminBundle/Controller/ProductController.php
+++ src/AdminBundle/Controller/ProductController.php
@@ -10,6 +10,7 @@ namespace AdminBundle\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
 class ProductController extends Controller
 {
@@ -17,5 +18,30 @@ class ProductController extends Controller
     {
         return $this->render('AdminBundle:Product:index.html.twig');
     }
+    public function addArticleAction(Request $request)
+    {
 
+        $article = new Article();
+        $form   = $this->get('form.factory')->create(ArticlesType::class, $article);
+
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+            // Ajoutez cette ligne :
+            // c'est elle qui déplace l'image là où on veut les stocker
+            $article->getImage()->upload();
+
+
+
+            $article->setPublished(new \Datetime());
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($article);
+            $em->flush();
+            return $this->redirectToRoute('hn_admin_menu');
+
+        }
+        return $this->render('HNAdminBundle:Default:add.html.twig', array(
+            'form' => $form->createView(),
+        ));
+
+    }
 }
