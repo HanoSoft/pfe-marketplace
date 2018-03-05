3a238a23b91fc0b7b571e1b8dfa1df72f16d81cf
Nouha <nouha.bensalem1992@gmail.com>
2018-03-05 17:31:30 +0100

    add the deleteAction() for the categoryController

--- src/AdminBundle/Controller/CategoryController.php
+++ src/AdminBundle/Controller/CategoryController.php
@@ -86,4 +86,35 @@ public function addAction(Request $request)
     ));
   }
 
+
+
+public function deleteAction(Request $request, $id)
+  {
+    $em = $this->getDoctrine()->getManager();
+
+   // 3a ya5a  ba3d mayjibha na3ml appel lel methode setDeleted(false) ma3mltch 7aja o5ra khow notion poo 7obi appel lel methode hathika hya wkbh bhi mch lbhi chno na3l bha fhma la la ey mthbta ey fhmt bhi taw kent fog 
+    $category  = $em->getRepository('AdminBundle:Category')->find($id);
+    $category->setDeleted(true);
+    
+
+    if (null === $category) {
+      throw new NotFoundHttpException("La categorie  ".$id." n'existe pas.");
+    }
+
+    // On crée un formulaire vide, qui ne contiendra que le champ CSRF
+    // Cela permet de protéger la suppression d'annonce contre cette faille
+    $form = $this->get('form.factory')->create();
+
+    if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+
+         $em->flush();
+   return $this->redirectToRoute('admin_category_show');
+    }
+    
+    return $this->render('AdminBundle:Category:delete.html.twig', array(
+      'category' => $category,
+      'form'   => $form->createView(),
+    ));
+  }
+
 }
--- src/AdminBundle/Repository/categoryRepository.php
+++ src/AdminBundle/Repository/categoryRepository.php
@@ -26,4 +26,6 @@ class categoryRepository extends \Doctrine\ORM\EntityRepository
 
 
 	}
+
+    
 }
