8ef8c871002078c80b4dc1005c74418c21595508
pophamdi <pophamdi@gmail.com>
2018-03-06 21:05:56 +0100

    create the fixture load for category and product

--- src/CoreBundle/DataFixtures/ORM/LoadProduct.php
+++ src/CoreBundle/DataFixtures/ORM/LoadProduct.php
@@ -6,9 +6,44 @@ namespace CoreBundle\DataFixtures\ORM;
 use Doctrine\Common\DataFixtures\FixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
 use CoreBundle\Entity\Product;
+use CoreBundle\Entity\Category;
 
-class LoadProduct
+
+class LoadProduct implements FixtureInterface
 {
+    public function load(ObjectManager $manager)
+    {
+        $names = array(
+            'Vêtements',
+            'Beauté et Parfums',
+            'DVD & Blu-ray',
+            'High-Tech',
+            'Chaussures',
+            'VOYAGES'
+        );
+
+        foreach ($names as $name) {
+
+            $category = new Category();
+            $category->setName($name);
+            $category->setDeleted(false);
+            $manager->persist($category);
+
+            for ($i=0;$i<10;$i++){
+                $product=new Product();
+                $product->setCategory($category);
+                $product->setProductName('produit' .$i);
+                $product->setPrice(10);
+                $product->setProductColor('noir');
+                $product->setProductDetails('description de produit'. $i);
+                $product->setQuantity(120);
+                $product->setDeleted(false);
+                $manager->persist($product);
+            }
+        }
 
 
+        $manager->flush();
+
+    }
 }
