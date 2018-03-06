e8f7a2631f7ab32e1f84d7f001a5b13b076b774f
pophamdi <pophamdi@gmail.com>
2018-03-06 18:52:16 +0100

    fix the service issue for the fixtureBundle

---
+++ src/AdminBundle/DataFixtures/ORM/LoadUser.php
@@ -0,0 +1,41 @@
+<?php
+
+
+namespace AdminBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use AdminBundle\Entity\User;
+
+use Symfony\Component\DependencyInjection\ContainerAwareInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+
+class LoadUser implements FixtureInterface,ContainerAwareInterface
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    /**
+     * {@inheritDoc}
+     */
+    public function setContainer(ContainerInterface $container = null) {
+        $this->container = $container;
+    }
+
+    public function load(ObjectManager $manager)
+    {
+        $userManager = $this->container->get('fos_user.user_manager');
+
+        $user = $userManager->createUser();
+        $user->setUsername('pophamdi');
+        $user->setEmail('pophamdi@gmail.com');
+        $user->setPlainPassword('pop1993.');
+        $user->setEnabled(true);
+        $user->addRole('ROLE_ADMIN');
+        $manager->persist($user);
+        $manager->flush();
+    }
+}
