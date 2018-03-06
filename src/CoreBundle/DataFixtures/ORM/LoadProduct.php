51a1dfa28b1d294a449e09ca3832d987d550f01f
Nouha <nouha.bensalem1992@gmail.com>
2018-03-06 19:06:48 +0100

    merge master branch

---
+++ src/CoreBundle/DataFixtures/ORM/LoadProduct.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace CoreBundle\DataFixtures\ORM;
+
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use CoreBundle\Entity\Product;
+
+class LoadProduct
+{
+
+
+}
