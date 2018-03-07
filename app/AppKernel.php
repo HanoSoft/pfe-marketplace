e76f378ae1bd7aac843355a340e7bfbadd59a200
pophamdi <pophamdi@gmail.com>
2018-03-07 12:04:37 +0100

    create the other fixture user

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -21,6 +21,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new CoreBundle\CoreBundle(),
             new Knp\Bundle\PaginatorBundle\KnpPaginatorBundle(),
+
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
