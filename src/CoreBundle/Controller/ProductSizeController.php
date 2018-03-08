64cbf99a136e76570173030948acddd13900b376
pophamdi <pophamdi@gmail.com>
2018-03-08 19:07:32 +0100

    generate productSize form

--- src/CoreBundle/Controller/ProductSizeController.php
+++ src/CoreBundle/Controller/ProductSizeController.php
@@ -7,9 +7,31 @@
  */
 
 namespace CoreBundle\Controller;
-
-
-class ProductSizeController
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use CoreBundle\Entity\ProductSize;
+class ProductSizeController extends Controller
 {
+    public function addAction($id, Request $request){
+        $em = $this->getDoctrine()->getManager();
+        $size = new ();
+        $form = $this->get('form.factory')->create(ImageType::class, $image);
+        $product = $em->getRepository('CoreBundle:Product')->find($id);
+        $image->setProduct($product);
+
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+
+            $image->setDeleted(false);
+            $image->upload();
+
+            $em->persist($image);
+            $em->flush();
+
+
+        }
+        return $this->render('CoreBundle:Image:add.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
 
 }
---
+++ src/CoreBundle/Form/ProductSizeType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace CoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductSizeType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('size')->add('type')->add('deleted')->add('product');
+    }/**
+     * {@inheritdoc}
+     */
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'CoreBundle\Entity\ProductSize'
+        ));
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getBlockPrefix()
+    {
+        return 'corebundle_productsize';
+    }
+
+
+}
