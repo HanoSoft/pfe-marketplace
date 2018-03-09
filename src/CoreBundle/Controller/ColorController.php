e313465e9052538aaf9f013c96c3b4f644f784e3
Nouha <nouha.bensalem1992@gmail.com>
2018-03-09 18:06:35 +0100

    add the showAction for the ColorController

--- src/CoreBundle/Controller/ColorController.php
+++ src/CoreBundle/Controller/ColorController.php
@@ -1,114 +1,33 @@
 <?php
 
+
 namespace CoreBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use CoreBundle\Form\CategoryType;
+use CoreBundle\Form\ColorType;
 use Symfony\Component\HttpFoundation\Request;
-use CoreBundle\Entity\Category;
-use CoreBundle\Form\CategoryEditType;
-
-
-
-class CategoryController extends Controller
-{
-        public function addAction(Request $request)
-    {
-        $category = new category();
-
-
-        $form   = $this->get('form.factory')->create(CategoryType::class, $category);
-
-        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
-            
-
-
-            $em = $this->getDoctrine()->getManager();
-            $em->persist($category);
-            $em->flush();
-            return $this->redirectToRoute('admin_category_show');
-
-        }
-        return $this->render('CoreBundle:Category:add.html.twig', array(
-            'form' => $form->createView(),
-        ));    }
-
+use CoreBundle\Entity\Color;
+use CoreBundle\Form\ColorEditType;
 
 
         public function showAction(Request $request)
     {
-            $repository = $this->getDoctrine()
-            ->getManager()->
-            getRepository('CoreBundle:Category');
-            $category = $repository->getAllCategories(false);
+              $repository = $this->getDoctrine()
+              ->getManager()->
+              getRepository('CoreBundle:Color');
+              $color = $repository->getAllColors(false);
 
-
-        /**
-         * @var $paginator\knp\component\Pager\Paginator
-         */
-            $paginator=$this->get('knp_paginator');
-            $pagination=$paginator->paginate(
-            $category,
-            $request->query->getInt('page',1),
-            $request->query->getInt('limit',5)
+          /**
+           * @var $paginator\knp\component\Pager\Paginator
+           */
+              $paginator=$this->get('knp_paginator');
+              $pagination=$paginator->paginate(
+              $category,
+              $request->query->getInt('page',1),
+              $request->query->getInt('limit',5)
         );
 
-
-
-        return $this->render('CoreBundle:Category:index.html.twig', array(
-            'categories' => $pagination,
+        return $this->render('CoreBundle:Color:index.html.twig', array(
+            'colors' => $pagination,
             ));
     }
 
-
-    /*modifier categorie*/
-
-         public function editAction($id, Request $request)
-  {
-              $em = $this->getDoctrine()->getManager();
-
-              $category = $em->getRepository('CoreBundle:Category')->find($id);
-
-
-              $form = $this->get('form.factory')->create(CategoryEditType::class, $category);
-
-               if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
-                // Inutile de persister ici, Doctrine connait déjà notre annonce
-              $em->flush();
-                return $this->redirectToRoute('admin_category_show');}
-
-                 return $this->render('CoreBundle:Category:edit.html.twig', array(
-                    'category' => $category,
-                    'form'   => $form->createView(),
-    ));
-  }
-
-
-
-        public function deleteAction(Request $request, $id)
-  {
-          $em = $this->getDoctrine()->getManager();
-          $category  = $em->getRepository('CoreBundle:Category')->find($id);
-          $category->setDeleted(true);
-
-      
-            if (null === $category) {
-              throw new NotFoundHttpException("La categorie  ".$id." n'existe pas.");
-                  }
-
-            // On crée un formulaire vide, qui ne contiendra que le champ CSRF
-           // Cela permet de protéger la suppression d'annonce contre cette faille
-            $form = $this->get('form.factory')->create();
-
-            if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
-              $em->flush();
-
-                return $this->redirectToRoute('admin_category_show');
-              }
-        
-            return $this->render('CoreBundle:Category:delete.html.twig', array(
-              'category' => $category,
-              'form'   => $form->createView(),
-        ));
-  }
-
-}
