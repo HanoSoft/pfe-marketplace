a873e8f60cc60f9d07178fdf2ec9618170688277
Nouha <nouha.bensalem1992@gmail.com>
2018-03-09 21:24:47 +0100

    add the addAction to the ColorController

--- src/CoreBundle/Controller/ColorController.php
+++ src/CoreBundle/Controller/ColorController.php
@@ -32,4 +32,27 @@ class ColorController extends Controller
             ));
     }
 
+
+        public function addAction(Request $request)
+        {
+            $color = new Color();
+
+
+        $form   = $this->get('form.factory')->create(ColorType::class, $color);
+
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+            
+
+
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($color);
+            $em->flush();
+            return $this->redirectToRoute('admin_color_show');
+
+        }
+        return $this->render('CoreBundle:Color:add.html.twig', array(
+            'form' => $form->createView(),
+        ));    }
+
+
 }
