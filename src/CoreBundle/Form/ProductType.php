90ead44fa384162ecb1d77193679bb30332a5c48
pophamdi <pophamdi@gmail.com>
2018-03-09 11:19:38 +0100

    create the stylebutton for add action (Product)

--- src/CoreBundle/Form/ProductType.php
+++ src/CoreBundle/Form/ProductType.php
@@ -4,6 +4,7 @@ namespace CoreBundle\Form;
 
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -16,7 +17,7 @@ class ProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder->add('productName')
-            ->add('price')
+            ->add('price',NumberType ::class)
             ->add('quantity')
             ->add('productDetails',TextareaType::class)
             ->add('productColor')
