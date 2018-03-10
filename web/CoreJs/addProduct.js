1595e0437334067446136e14d2a81dc13bd17ab0
pophamdi <pophamdi@gmail.com>
2018-03-10 15:26:52 +0100

    update add image form with validation

--- web/CoreJs/addProduct.js
+++ web/CoreJs/addProduct.js
@@ -131,6 +131,51 @@ function checkSize(){
 
 }
 
+
 $(".size").on('input',function() {
     alertSize('size','containerSize','sizeHelp','sizeAlertIcon',1);
 });
+
+// chek the image
+var label,file=0;
+function alertImage(input,container,help,alertIcon,limit){
+
+    var  size=$("."+input).val();
+
+    if (size.length <limit)
+    {
+        $('.'+container).addClass('has-warning has-feedback');
+        $('#'+help).removeClass('sr-only');
+        $('#'+alertIcon).addClass('glyphicon-warning-sign').removeClass('glyphicon-ok');
+
+       label=0;
+
+
+        checkImage();
+    }
+
+    else{
+        $('.'+container).addClass('has-success has-feedback');
+        $('#'+help).addClass('sr-only');
+        $('#'+alertIcon).addClass('glyphicon-ok').removeClass('glyphicon-warning-sign');
+
+        if (input==="labelImage"){label=1;}
+
+        checkImage();
+    }
+}
+function checkImage() {
+    if (label > 0 ) {
+        $('#btnAddImage').removeClass('disabled');
+    }
+    else {
+        $('#btnAddImage').addClass('disabled');
+    }
+}
+
+$(".labelImage").on('input',function() {
+    alertImage('labelImage','containerLabelImage','LabelImageHelp','LabelImageAlertIcon',1);
+});
+
+
+
