8878043c1f264cf3e8adc9632ec1619b21d08bc5
pophamdi <pophamdi@gmail.com>
2018-03-12 12:11:57 +0100

    create the ProductEditType

---
+++ src/CoreBundle/Form/ProductEditType.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace CoreBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+
+class ProductEditType extends AbstractType
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+
+    }
+
+    public function getParent()
+    {
+        return ProductType::class;
+    }
+
+
+}
