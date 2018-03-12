cd67ea7b18f2a6f26389b02f9746471186566272
pophamdi <pophamdi@gmail.com>
2018-03-12 14:33:17 +0100

    update the editAction for imagefa

--- web/CoreJs/Product/Prodcut.js
+++ web/CoreJs/Product/Prodcut.js
@@ -1,15 +1,4 @@
-//code modal js
-$('#myModal').on('show.bs.modal', function (event) {
-    var button = $(event.relatedTarget) ;// Button that triggered the modal
-    var id= button.data('wathever'); // Extract info from data-* attributes
 
-    var modal = $(this);
-
-    modal.find('#form').attr("action",id);
-
-
-});
-// end modal
 
 //methode fir the help
 var name,details,quantity,price =0;
