8b5c7a8dc14a9b0591b5e180a545a2d785d80b76
Nouha <nouha.bensalem1992@gmail.com>
2018-03-17 15:16:57 +0100

    change the locale to fr

--- src/AdminBundle/Controller/TranslateController.php
+++ src/AdminBundle/Controller/TranslateController.php
@@ -5,7 +5,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class TranslateController extends Controller
 {
-    public function indexAction()
+    public function translateAction()
     {
         return $this->render('AdminBundle:Admin:leftSideNav.html.twig');
     }
