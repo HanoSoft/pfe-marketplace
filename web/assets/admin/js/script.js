2c0a5dd008ff159816629373c9bbb32148f17c85
pophamdi <pophamdi@gmail.com>
2018-03-17 17:40:11 +0100

    create the form validation

--- web/assets/admin/js/script.js
+++ web/assets/admin/js/script.js
@@ -5,3 +5,37 @@ $('#myModal').on('show.bs.modal', function (event) {
     var modal = $(this);
     modal.find('#form').attr("action",id);
 });
+// form validation
+
+
+    $.validator.setDefaults({
+        highlight: function(element) {
+            $(element).closest('.form-group').removeClass('has-success').addClass('has-warning');
+            $(element).closest('.form-group').find('span.glyphicon').removeClass('glyphicon-ok').addClass('glyphicon-warning-sign');
+            $(element).closest('.form-group').find('span.glyphicon').effect( "slide" );
+        },
+        unhighlight: function(element) {
+            $(element).closest('.form-group').removeClass('has-warning').addClass('has-success');
+            $(element).closest('.form-group').find('span.glyphicon').removeClass('glyphicon-warning-sign').addClass('glyphicon-ok');
+        },
+        errorElement: 'span',
+        errorClass: 'help-block',
+        errorPlacement: function(error, element) {
+            if(element.parent('.input-group').length) {
+                error.insertAfter(element.parent());
+            } else {
+                error.insertAfter(element);
+            }
+        }
+    });
+
+$(".form").validate();
+$('.form input').on('keyup blur', function () {
+    if ($('.form').valid()) {
+        $('button.btn').prop('disabled', false);
+    }
+    else {
+        $('button.btn').prop('disabled', 'disabled');
+    }
+});
+
