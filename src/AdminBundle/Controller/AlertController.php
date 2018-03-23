ae6608ee48ca406567d2311d009d31b57b9c8e5c
pophamdi <pophamdi@gmail.com>
2018-03-23 13:51:39 +0100

    initial the alert controller

---
+++ src/AdminBundle/Controller/AlertController.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hamdi
+ * Date: 23/03/2018
+ * Time: 13:50
+ */
+
+namespace AdminBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class AlertController extends Controller
+{
+
+}
