04383d55e0ba8b22ca4c4987b3456517178d62d2
Nouha <nouha.bensalem1992@gmail.com>
2018-03-23 15:03:36 +0100

    update the services.yml

--- src/AdminBundle/Controller/BrandController.php
+++ src/AdminBundle/Controller/BrandController.php
@@ -13,5 +13,22 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class BrandController extends Controller
 {
+    public function indexAction()
+    {
+        return $this->render('AdminBundle:Brand:index.html.twig');
+    }
+    public function addAction(Request $request)
+    {
 
+        $form   = $this->get('form.factory')->create(BrandType::class);
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+            $manager = $this->get('core.service.brand_manager');
+            $manager->add($form);
+
+            return $this->redirectToRoute('admin_brand_add');
+        }
+        return $this->render('AdminBundle:Brand:add.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
 }
