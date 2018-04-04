7506f8e071c772f761d8379e7ffd95ca24b26b2a
pophamdi <pophamdi@gmail.com>
2018-04-04 21:26:50 +0100

    update imageController

--- src/AdminBundle/Controller/ImageController.php
+++ src/AdminBundle/Controller/ImageController.php
@@ -8,6 +8,7 @@ use CoreBundle\Entity\Image;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\Session;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class ImageController extends Controller
 {
@@ -43,35 +44,65 @@ class ImageController extends Controller
             'form' => $form->createView(),
         ));
     }
-
-
-    /*
-     *
-
-
     public function editAction(Request $request ,$idp,$id)
     {
-        $manager = $this->get('core.service.image_manager');
-        $image=$manager->find($id);
+        $em = $this->getDoctrine()->getManager();
+        $serviceImage = $this->get('core.service.image');
+        $image=$serviceImage->getImage($id);
         $form = $this->get('form.factory')->create(ImageEditType::class,$image);
         if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
-            $manager->edit($form,$id);
+            $em->flush();
             return $this->redirectToRoute('admin_product_image_list',array('id' => $idp));
         }
         return $this->render('AdminBundle:Image:edit.html.twig', array(
             'form' => $form->createView(),
         ));
     }
-    public function deleteAction(Request $request,$idp,$id)
+    public function disableAction(Request $request,$idp,$id)
     {
         $formDelete = $this->get('form.factory')->create();
         if ($request->isMethod('POST') && $formDelete->handleRequest($request)->isValid()) {
-            $manager = $this->get('core.service.image_manager');
-            $manager->delete($id);
+            $serviceImage = $this->get('core.service.image');
+            $em = $this->getDoctrine()->getManager();
+            $image=$serviceImage->getImage($id);
+            $image->setDeleted(true);
+            $em->flush();
             return $this->redirectToRoute('admin_product_image_list',array('id' => $idp));
         }
         return $this->render('AdminBundle::delete.html.twig', array(
             'formDelete'   => $formDelete->createView(),
         ));
-    }*/
+    }
+    public function enableAction(Request $request,$idp,$id)
+    {
+        $formDelete = $this->get('form.factory')->create();
+        if ($request->isMethod('POST') && $formDelete->handleRequest($request)->isValid()) {
+            $serviceImage = $this->get('core.service.image');
+            $em = $this->getDoctrine()->getManager();
+            $image=$serviceImage->getImage($id);
+            $image->setDeleted(false);
+            $em->flush();
+            return $this->redirectToRoute('admin_product_image_list',array('id' => $idp));
+        }
+        return $this->render('AdminBundle::delete.html.twig', array(
+            'formDelete'   => $formDelete->createView(),
+        ));
+    }
+    public function deleteAction(Request $request,$idp,$id){
+        $em=$this->getDoctrine()->getManager();
+        $serviceImage =$this->get('core.service.image');
+        $image=$serviceImage->getImage($id);
+        if (null === $image) {
+            throw new NotFoundHttpException("L'image  de l'id ".$id." n'existe pas.");
+        }
+        $formDelete = $this->get('form.factory')->create();
+        if ($request->isMethod('POST') && $formDelete->handleRequest($request)->isValid()) {
+            $em->remove($image);
+            $em->flush();
+            return $this->redirectToRoute('admin_product_image_list',array('id' => $idp));
+        }
+        return $this->render('AdminBundle::delete.html.twig', array(
+            'formDelete' => $formDelete->createView(),
+        ));
+    }
 }
