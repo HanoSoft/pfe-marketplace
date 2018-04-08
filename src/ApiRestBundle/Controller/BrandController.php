8af9475368050458ac3a151cccc9bd58cbd5090f
pophamdi <pophamdi@gmail.com>
2018-04-08 10:58:03 +0100

    create brand Api

---
+++ src/ApiRestBundle/Controller/BrandController.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Hamdi
+ * Date: 08/04/2018
+ * Time: 10:44
+ */
+
+namespace ApiRestBundle\Controller;
+
+use FOS\RestBundle\Controller\Annotations\Get;
+use FOS\RestBundle\Controller\Annotations\View;
+use FOS\RestBundle\Controller\FOSRestController;
+
+class BrandController extends FOSRestController
+{
+    /**
+     * @Get(
+     *     path = "/api/brands",
+     *     name = "api_brand_list",
+     *
+     * )
+     * @View
+     */
+    public function indexAction()
+    {
+        $brandService=$this->get('core.service.brand');
+        $brands=$brandService->getActiveBrands();
+        return $brands;
+    }
+
+}
