13bd64502fdf68c9d8636bc94a4a9050aa96fc27
Nouha <nouha.bensalem1992@gmail.com>
2018-05-08 14:47:22 +0100

    add the service item

--- src/CoreBundle/Service/ItemService.php
+++ src/CoreBundle/Service/ItemService.php
@@ -3,13 +3,13 @@
  * Created by PhpStorm.
  * User: nouha
  * Date: 08/05/2018
- * Time: 14:44
+ * Time: 14:46
  */
 
 namespace CoreBundle\Service;
 
 
-class ServiceItem
+class ItemService
 {
 
 }
