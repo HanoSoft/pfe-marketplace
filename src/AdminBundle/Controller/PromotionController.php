ad2b80d65f56887dafd5ddb06875a34e4f5c6f7b
Nouha <nouha.bensalem1992@gmail.com>
2018-05-16 11:51:12 +0100

    create the add action to the promotion

--- src/AdminBundle/Controller/PromotionController.php
+++ src/AdminBundle/Controller/PromotionController.php
@@ -8,7 +8,11 @@
 
 namespace AdminBundle\Controller;
 
+use CoreBundle\Entity\Promotion;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use CoreBundle\Form\PromotionType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class PromotionController extends Controller
 {
@@ -22,5 +26,37 @@ class PromotionController extends Controller
             'formDelete'   => $formDelete->createView(),
         ));
     }
+    public function addAction(Request $request)
+    {
+        $promotion=new Promotion();
+        $session = new Session();
+        $form = $this->createForm(PromotionType::class,$promotion);
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($promotion);
+            $em->flush();
+            $session->getFlashBag()->add('success', 'la livraison est bien enregistrée !');
+            return $this->redirectToRoute('admin_delivery_add');
+        }
+        return $this->render('AdminBundle:Promotion:add.html.twig', array(
+            'form' => $form->createView(),
+        ));
+
+    }
+    public function editAction(Request $request ,$id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $servicePromotion = $this->get('core.service.promotion');
+        $promotion=$servicePromotion->getPromotion($id);
+        $form = $this->get('form.factory')->create(PromotionEditType::class,$promotion);
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()) {
+            $em->flush();
+            return $this->redirectToRoute('admin_promotion_list');
+        }
+        return $this->render('AdminBundle:Promotion:edit.html.twig', array(
+            'form' => $form->createView(),
+        ));
+    }
+
 
 }
---
+++ src/CoreBundle/Form/PromotionEditType.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: nouha
+ * Date: 16/05/2018
+ * Time: 11:50
+ */
+
+namespace CoreBundle\Form;
+
+
+class PromotionEditType
+{
+
+}
